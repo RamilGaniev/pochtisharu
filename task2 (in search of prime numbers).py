aa436546c5416d851e1601bdf9fe0ba727e8f64f
Ramil <ramil@MacBook-Air-Ramil.local>
2023-07-01 09:37:25 +0300

    Добавил дз и поменял имена файлов

---
+++ task2 (in search of prime numbers).py
@@ -0,0 +1,22 @@
+import math
+elem = 0
+def primesearcher(elem):
+    collector = []
+    output = []
+    collector.append(elem)
+    for i in range(45):
+        if elem % 2 == 0:
+            elem += 21
+        else:
+            elem -= 1
+        collector.append(elem)
+    for elemi in collector:
+        flag = 0
+        for devider in range(2, int(math.sqrt(elem))+1):
+            if elemi % devider == 0:
+                flag = 1
+        if flag == 0:
+            output.append(elemi)
+    return(output)
+print(primesearcher(elem))
+
